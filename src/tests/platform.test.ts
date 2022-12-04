75ca082982e2a4817505ba78ed3d8104c42ed8a3
Jordy van den Aardweg <jordyvandenaardweg@gmail.com>
2022-12-04 23:53:56 +0100

    test: add some platform unit tests

--- src/tests/platform.test.ts
+++ src/tests/platform.test.ts
@@ -1,5 +1,60 @@
+import { HomebridgeAPI } from "homebridge/lib/api";
+import { HomebridgeHomeWizardEnergySocket } from "../platform";
+import { PLATFORM_NAME } from "../settings";
+import { TxtRecord } from "../types";
+
+// mock the logger parameter
+const loggerMock = {
+  info: jest.fn(),
+  warn: jest.fn(),
+  error: jest.fn(),
+  debug: jest.fn(),
+  log: jest.fn(),
+};
+
+// mock the config parameter
+const configMock = {
+  platform: PLATFORM_NAME,
+};
+
+const apiMock = new HomebridgeAPI();
+
+const platform = new HomebridgeHomeWizardEnergySocket(
+  loggerMock,
+  configMock,
+  apiMock
+);
+
 describe("platform", () => {
-  it("should be true", () => {
-    expect(true).toBe(true);
+  it('should return true if api_enabled is "1"', () => {
+    const txtRecordMock = {
+      api_enabled: "1",
+    } as TxtRecord;
+
+    expect(platform.isDeviceApiEnabled(txtRecordMock)).toBe(true);
+  });
+
+  it('should return false if api_enabled is not "1"', () => {
+    const txtRecordMock = {
+      api_enabled: "0",
+    } as TxtRecord;
+
+    expect(platform.isDeviceApiEnabled(txtRecordMock)).toBe(false);
+  });
+
+  it('should return true if product_type is "HWE-SKT"', () => {
+    const txtRecordMock = {
+      product_type: "HWE-SKT",
+    } as TxtRecord;
+
+    expect(platform.isDeviceProductTypeSupported(txtRecordMock)).toBe(true);
+  });
+
+  it('should return false if product_type is not "HWE-SKT"', () => {
+    const txtRecordMock = {
+      product_type: "something-else",
+    } as TxtRecord;
+
+    expect(platform.isDeviceProductTypeSupported(txtRecordMock)).toBe(false);
   });
 });
