14a2510e80490f95d1b1762ebeabf07b881652ab
Jordy van den Aardweg <jordyvandenaardweg@gmail.com>
2022-12-11 01:07:28 +0100

    fix: linter error preventing release

--- src/energySocketAccessory.ts
+++ src/energySocketAccessory.ts
@@ -11,7 +11,6 @@ import {
   EnergySocketAccessoryProperties,
   HomeWizardApiBasicInformationResponse,
   HomeWizardApiIdentifyResponse,
-  HomeWizardApiStatePutParams,
   HomeWizardApiStateResponse,
   HomeWizardEnergyPlatformAccessoryContext,
   PLATFORM_MANUFACTURER,
