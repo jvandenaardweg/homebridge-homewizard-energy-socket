509d8e2c6297e5e9e7dff42a606e283ac8ff7640
Jordy van den Aardweg <jordyvandenaardweg@gmail.com>
2022-12-14 13:17:37 +0100

    refactor: use schema validation using zod to check if config is valid

--- src/config.schema.ts
+++ src/config.schema.ts
@@ -12,67 +12,84 @@ export const DEFAULT_OUTLETINUSE_THRESHOLD_DURATION = 10;
 export const DEFAULT_OUTLETINUSE_THRESHOLD_DURATION_MIN = 0;
 export const DEFAULT_OUTLETINUSE_THRESHOLD_DURATION_MAX = 86400;
 
-// validate the config schema from config.schema.json
+// this schema should match the config.schema.json
+// using zod to validate the config gives us type-safety over the config in our code
 export const configSchema = z.object({
-  name: z.string(),
-  energySockets: z.array(
-    z.object({
-      ip: z
-        .string({
-          required_error: 'IP address is required for each Energy Socket',
-        })
-        .refine(
-          ip => isIP(ip, 4),
-          ip => ({
-            message: `${ip} is not a valid IPv4 address`,
-          }),
-        ),
-      name: z.string({
-        required_error: 'Name is required for each Energy Socket',
-      }),
-      outletInUse: z
-        .object({
-          isActive: z.boolean().optional(),
-          threshold: z
-            .number()
-            .min(DEFAULT_OUTLETINUSE_THRESHOLD_MIN)
-            .max(DEFAULT_OUTLETINUSE_THRESHOLD_MAX)
-            .optional(), // set as optional, because it is only required when outletInUse.isActive is true. We'll use superRefine to validate this
-          thresholdDuration: z
-            .number()
-            .min(DEFAULT_OUTLETINUSE_THRESHOLD_DURATION_MIN)
-            .max(DEFAULT_OUTLETINUSE_THRESHOLD_DURATION_MAX)
-            .optional(), // set as optional, because it is only required when outletInUse.isActive is true. We'll use superRefine to validate this
-        })
-
-        .superRefine((schema, ctx) => {
-          const isActive = schema?.isActive;
-          const threshold = schema?.threshold;
-          const thresholdDuration = schema?.thresholdDuration;
-
-          // If isActive is true, threshold and thresholdDuration are required
-          if (isActive) {
-            if (!threshold) {
-              ctx.addIssue({
-                code: z.ZodIssueCode.custom,
-                path: ['threshold'],
-                message: 'A threshold is required when outletInUse.isActive is true',
-              });
-            }
-
-            if (!thresholdDuration) {
-              ctx.addIssue({
-                code: z.ZodIssueCode.custom,
-                path: ['thresholdDuration'],
-                message: 'A thresholdDuration is required when outletInUse.isActive is true',
-              });
-            }
-          }
-
-          return z.NEVER;
+  name: z.string({
+    required_error: 'A bridge name is required',
+  }),
+  energySockets: z
+    .array(
+      z.object({
+        ip: z
+          .string({
+            required_error: 'IP address is required for each Energy Socket',
+            invalid_type_error: "'ip' must be a string",
+          })
+          .refine(
+            ip => isIP(ip, 4),
+            ip => ({
+              message: `'${ip}' is not a valid IPv4 address`,
+            }),
+          ),
+        name: z.string({
+          required_error: 'Name is required for each Energy Socket',
+          invalid_type_error: "'name' must be a string",
         }),
-    }),
-  ),
+        outletInUse: z
+          .object({
+            isActive: z.boolean().optional(),
+            threshold: z
+              .number({
+                invalid_type_error: "'threshold' must be a number",
+              })
+              .min(DEFAULT_OUTLETINUSE_THRESHOLD_MIN)
+              .max(DEFAULT_OUTLETINUSE_THRESHOLD_MAX)
+              .optional(), // set as optional, because it is only required when outletInUse.isActive is true. We'll use superRefine to validate this
+            thresholdDuration: z
+              .number({
+                invalid_type_error: "'thresholdDuration' must be a number",
+              })
+              .min(DEFAULT_OUTLETINUSE_THRESHOLD_DURATION_MIN)
+              .max(DEFAULT_OUTLETINUSE_THRESHOLD_DURATION_MAX)
+              .optional(), // set as optional, because it is only required when outletInUse.isActive is true. We'll use superRefine to validate this
+          })
+
+          .superRefine((schema, ctx) => {
+            const isActive = schema?.isActive;
+            const threshold = schema?.threshold;
+            const thresholdDuration = schema?.thresholdDuration;
+
+            // If isActive is true, threshold and thresholdDuration are required
+            if (isActive) {
+              if (!threshold) {
+                ctx.addIssue({
+                  code: z.ZodIssueCode.custom,
+                  path: ['threshold'],
+                  message: 'A threshold is required when outletInUse.isActive is true',
+                });
+              }
+
+              if (!thresholdDuration) {
+                ctx.addIssue({
+                  code: z.ZodIssueCode.custom,
+                  path: ['thresholdDuration'],
+                  message: 'A thresholdDuration is required when outletInUse.isActive is true',
+                });
+              }
+            }
+
+            return z.NEVER;
+          }),
+      }),
+    )
+    .optional(),
+  // .refine(
+  //   array => Array.isArray(array) && array.length === 0,
+  //   () => ({
+  //     message: `Energy Sockets array cannot be empty. Either remove the array or add at least one Energy Socket`,
+  //   }),
+  // ),
 });
 
 export type ConfigSchema = z.infer<typeof configSchema> & PlatformConfig;
